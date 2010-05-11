deddf92f749482b64eede9f3debcae0379806a4f
James.M.Martin <James.M.Martin@a868a8c5-572c-0410-a3af-31ff0a21ac75>
2010-05-11 03:04:06 +0000

    Added RETS version revision support.
    git-svn-id: https://jrets.googlecode.com/svn/trunk@25 a868a8c5-572c-0410-a3af-31ff0a21ac75

--- rets-client/dev/src/java/org/realtors/rets/client/RetsVersion.java
+++ rets-client/dev/src/java/org/realtors/rets/client/RetsVersion.java
@@ -8,22 +8,32 @@ import org.apache.commons.lang.math.NumberUtils;
 public class RetsVersion implements Serializable {
 	public static final String RETS_VERSION_HEADER = "RETS-Version";
 
-	public static final RetsVersion RETS_10 = new RetsVersion(1, 0, 0);
-	public static final RetsVersion RETS_15 = new RetsVersion(1, 5, 0);
-	public static final RetsVersion RETS_16 = new RetsVersion(1, 6, 0);
-	public static final RetsVersion RETS_17 = new RetsVersion(1, 7, 0);
+	public static final RetsVersion RETS_10 = new RetsVersion(1, 0, 0, 0);
+	public static final RetsVersion RETS_15 = new RetsVersion(1, 5, 0, 0);
+	public static final RetsVersion RETS_16 = new RetsVersion(1, 6, 0, 0);
+	public static final RetsVersion RETS_17 = new RetsVersion(1, 7, 0, 0);
+	public static final RetsVersion RETS_1_7_2 = new RetsVersion(1, 7, 2, 0);
 	public static final RetsVersion DEFAULT = RETS_15;
 
 	private int mMajor;
 	private int mMinor;
+	private int mRevision;
 	private int mDraft;
 
 	public RetsVersion(int major, int minor) {
-		this(major,minor,0);
+		this(major,minor,0,0);
 	}
+	
+	/** @deprecated use <code>new RetsVersion(major, minor, 0, draft)</code> */
+	@Deprecated
 	public RetsVersion(int major, int minor, int draft) {
+		this(major,minor,0,draft);
+	}
+	
+	public RetsVersion(int major, int minor, int revision, int draft) {
 		this.mMajor = major;
 		this.mMinor = minor;
+		this.mRevision = revision;
 		this.mDraft = draft;
 	}
 
@@ -35,23 +45,33 @@ public class RetsVersion implements Serializable {
 		return this.mMinor;
 	}
 
+	public int getRevision() {
+		return this.mRevision;
+	}
+	
 	public int getDraft() {
 		return this.mDraft;
 	}
 
 	@Override
 	public String toString() {
-		if (this.mDraft == 0) {
-			return "RETS/" + this.mMajor + "." + this.mMinor;
+		if (this.mRevision == 0) {
+			if (this.mDraft == 0) {
+				return "RETS/" + this.mMajor + "." + this.mMinor;
+			}
+			return "RETS/" + this.mMajor + "." + this.mMinor + "d" + this.mDraft;
 		}
-		return "RETS/" + this.mMajor + "." + this.mMinor + "d" + this.mDraft;
+		if (this.mDraft == 0) {
+			return "RETS/" + this.mMajor + "." + this.mMinor + "." + this.mRevision;
+		}
+		return "RETS/" + this.mMajor + "." + this.mMinor + "." + this.mRevision + "d" + this.mDraft;
 	}
 
 	@Override
 	public boolean equals(Object o) {
 		if (o instanceof RetsVersion) {
 			RetsVersion v = (RetsVersion) o;
-			if ((v.getMajor() == this.mMajor) && (v.getMinor() == this.mMinor) && (v.getDraft() == this.mDraft)) {
+			if ((v.getMajor() == this.mMajor) && (v.getMinor() == this.mMinor) && (v.getRevision() == this.mRevision) && (v.getDraft() == this.mDraft)) {
 				return true;
 			}
 		}
@@ -62,9 +82,15 @@ public class RetsVersion implements Serializable {
 		if( StringUtils.isEmpty(ver) ) return null;
 		String[] split = StringUtils.trimToEmpty(ver).split("\\.");
 		int ma = NumberUtils.toInt(split[0],1);
-		int mn = split.length > 1 ? NumberUtils.toInt(split[1],0) : 0; 
-		int dr = split.length > 2 ? NumberUtils.toInt(split[2],0) : 0;
-		return new RetsVersion(ma,mn,dr);
+		int mn = split.length > 1 ? NumberUtils.toInt(split[1],0) : 0;
+		int re = 0;
+		int dr = 0;
+		if (split.length > 2) {
+			split = StringUtils.defaultString(split[2]).split("d");
+			re = NumberUtils.toInt(split[0],0);
+			dr = split.length > 1 ? NumberUtils.toInt(split[1],0) : 0;
+		}
+		return new RetsVersion(ma,mn,re,dr);
 	}
-
+	
 }
